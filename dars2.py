1767f1b6c0757d9b0ed3accf66a0e2d98d0c8038
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-01-17 17:18:37 +0500

    komyuterdan fayl qo'shildi
    bu funksiyasini ishlash uchun biz kopyuterdan fayl qo'shdik

---
+++ dars2.py
@@ -0,0 +1,41 @@
+from dars16 import Yegindi, Raqamlar
+
+a = 7898
+
+print(Raqamlar(a=a))
+print(Yegindi(89, 95))
+
+# r1, r2, r3 = map(int, input().split())
+
+# p = 3.14
+
+# s1 = p * r1 ** 2
+# s2 = p * r2 ** 2 
+# s3 = p * r3 ** 2 
+
+# print(s1, s2, s3)
+
+# a = int(input("soni kiriting: "))
+# b = int(input("soni kiriting: "))
+
+# s = a * b
+
+# print(s)
+
+
+
+# a = float(input("a: "))
+# b = int(input("b: "))
+# s = a / b
+# print("%.2f" % s)
+# a = 5
+# a = 5.6
+# a = "salom"
+# a = 'salom'
+# a = """Hayr 
+#                 maktab"""
+# a = True
+# a = 5+6j
+# print(a)
+# print(type(s))
+
